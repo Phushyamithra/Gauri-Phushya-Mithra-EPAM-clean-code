7fd4eeb4d2dee8f39861eddbe99f2a2d6b961aa2
Phushyamithra <60391390+Phushyamithra@users.noreply.github.com>
2020-02-20 21:58:56 +0530

    Add files via upload
    test

---
+++ Cleancode/src/main/java/com/mithra/Cleancode/Compound.java
@@ -0,0 +1,14 @@
+package com.mithra.Cleancode;
+
+import java.io.IOException;
+
+public class Compound 
+{
+	public static double Compound_interest(double principle_amt,double rate_of_interest,double time,double noof_times) throws IOException
+    {
+		double final_amt;
+    	final_amt=principle_amt*Math.pow((1+((rate_of_interest/100)/noof_times)),noof_times*time);
+    	return final_amt;
+    }
+}
+
